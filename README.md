0625592727308bd519162e2da54c106879783bea
Ariel Barrios <ariel29111978@hotmail.com>
2023-05-04 12:37:37 -0300

    Configura el store y realiza un contador

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # practica-toolkid-redux
+
+## Este solo es un proyecto de práctica. Temas a tratar:
+
+- Configuración del Store utilirando Redux Toolkit.
+- Como usar valores del store y despachar acciones.
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import './App.css'
+import { useDispatch, useSelector } from 'react-redux';
+
+import './App.css';
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+} from './store/slices/counter';
 
 function App() {
+  const { counter } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
 
   return (
-    <div>
-      <h1>Hola</h1>
+    <div className="card">
+      <p>count is {counter}</p>
+      <button onClick={() => dispatch(increment())}>increment</button>
+      <button onClick={() => dispatch(decrement())}>decrement</button>
+      <button onClick={() => dispatch(incrementByAmount(2))}>
+        increment 2
+      </button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
